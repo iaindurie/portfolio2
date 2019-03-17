a7f1b8237bb278149b798fdcfad7116fc6b934f1
Iain Durie <>
2019-03-17 12:54:02 +0000

    Typography

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,6 +2,7 @@ import React from "react"
 import { StaticQuery, graphql } from "gatsby"
 import Header from 'header'
 import Footer from 'footer'
+import "./layout.css"
 
 
 export default ({ children }) => (
